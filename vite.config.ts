a4d9e8df968fadf6339b69509165819af09a0f99
Nurmukhammad <sobirovn707@gmail.com>
2024-09-23 10:52:27 +0500

    chunk size updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { chunkSizeWarningLimit: 1000 },
   resolve: {
     alias: [
       { find: "@/", replacement: "/src/" },
