191782b74e04402b07c0ee8b8deda3e4d789108e
Nurmukhammad <sobirovn707@gmail.com>
2024-10-08 16:25:18 +0500

    adding excel download

---
+++ src/hooks/useComplaintExcel.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import baseApi from "@/api/baseApi";
+
+type Params = {
+  otk_status: string | number;
+  from_date: string;
+  to_date: string;
+  enabled?: boolean;
+};
+
+export const useComplaintExcel = ({ enabled = true, ...params }: Params) => {
+  console.log(params, "params");
+
+  return useQuery({
+    queryKey: ["complaints_excel", params],
+    queryFn: () =>
+      baseApi
+        .get("/complaints/excell", { params })
+        .then(({ data: response }) => response as { filename: string }),
+    enabled,
+  });
+};
+
+export default useComplaintExcel;
